c5fdcb09567a58fa26d7815fb8564310fcc95f47
venom21adi <69112627+venom21adi@users.noreply.github.com>
2023-06-18 08:09:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,12 +19,6 @@ import os
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-app.config.from_pyfile('config.py')
-account = app.config['ACCOUNT_NAME']   # Azure account name
-key = app.config['ACCOUNT_KEY']      # Azure Storage account access key  
-connect_str = app.config['CONNECTION_STRING']
-allowed_ext = app.config['ALLOWED_EXTENSIONS'] # List of accepted extensions
-max_length = app.config['MAX_CONTENT_LENGTH'] # Maximum size of the uploaded file
 
 
     
